e50cb7e1fafc4a25f39515b880d52a279f3d9335
Mansura Chowdhury <c.mansura@gmail.com>
2017-04-25 15:32:08 -0400

    changed dates

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -45,7 +45,7 @@ Thank you for registering for ICNB 2018. If you have requested any official lett
 
 If you are an author, please make sure to send us the final version of your paper and a signed copyright form via email to info@nbconference.com. You can find the copyright form here: www.nbconference.com/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
-To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: www.nbconference.com/accommodation/#0
+To reserve a room with a DISCOUNTED price, please fill out the provided booking form here: www.nbconference.com/accommodation/#Novotel
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@nbconference.com mentioning the correct information. Please note that you SHOULD NOT refill the form.
 
@@ -245,7 +245,7 @@ mail($your_email,$your_subject,$message,$your_headers);
     <div>
       <h2 class="title">1</h2>
       <p class="bold">ICNB 2018:</p>
-      <p class="body">ICNB 2018 will  be held in Budapest, Hungary on April 8 - 10, 2018 at the Novotel Budapest Centrum.</p>
+      <p class="body">ICNB 2018 will  be held in Budapest, Hungary on April 10 - 12, 2018 at the Novotel Budapest Centrum.</p>
 
       <!-- <p class="bold">RAN'18 Workshop</p>
       <p class="body">International ASET Inc. is proud to present <b>Dr. Vladimir A. Baulin</b> (Universitat Rovira I Virgili, Spain) and <b>Prof. Elena P. Ivanova</b> (Swinburne University of Technology, Australia) as organizers of a dedicated workshop for the 2nd World Congress on Recent Advances in Nanotechnology (RAN'18).</p>
@@ -324,7 +324,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <!-- <div class="past">Feb. 10, 2017</div> --> Sep. 28, 2017
+    <!-- <div class="past">Feb. 10, 2017</div> --> Oct. 12, 2017
   </div>
 </div>
 
@@ -338,7 +338,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
   <div class="date">
-    <!-- <div class="past">Feb. 10, 2017</div> --> Oct. 26, 2017
+    <!-- <div class="past">Feb. 10, 2017</div> --> Nov. 2, 2017
   </div>
 </div>
 
